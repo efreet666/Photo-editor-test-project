4d300279447a2d9ac38b0918fdbfa3fa67597ddb
Vlad <vladislav.bok@yandex.ru>
2022-05-14 23:42:56 +0300

    Initial Commit

---
+++ Photo editor test project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo editor test project
+//
+//  Created by Влад Бокин on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
